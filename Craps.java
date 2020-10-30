2fdef49b15b8388a370fad189725c6ab64d88c45
samsonc <samsonc0514@gmail.com>
2020-10-30 13:22:03 -0400

    Case3 internal simulation

--- Craps.java
+++ Craps.java
@@ -28,6 +28,33 @@ public class Craps{
             else if (num == 4 || num == 5 || num == 6 || num == 8 || num == 9 || num == 10){
                 System.out.println("Sum of first roll = " + num + ".   " + num +" is the \"point\".");
 
+                int point = 0;
+                int sum = 0;
+                while (point != 7 || sum != num) {
+                    point = rand.nextInt(13-1) + 1;
+                    System.out.println("Sum of next roll = " + point + ". ");
+                    sum = sum + point;
+                    point = rand.nextInt(13-1) + 1;
+                    System.out.println("Sum of next roll = " + point + ". ");
+                    sum = sum + point;
+
+                    if (point == 7) {
+                        System.out.print("Player loses.");
+                        System.out.println();
+                        System.out.println();
+                        System.out.print("Do you want to play again? ");
+
+                        ans = scan.nextLine();
+                    }
+                    else if (sum == num) {
+                        System.out.print("Player wins.");
+                        System.out.println();
+                        System.out.println();
+                        System.out.print("Do you want to play again? ");
+
+                        ans = scan.nextLine();
+                    }
+                }
             }
         } while (ans.equals("y"));
     }
